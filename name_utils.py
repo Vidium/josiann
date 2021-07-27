ab31de908e64ddf5a991f4a0818eefae270bfe2e
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2021-07-27 14:29:49 +0200

    initial version : convergence for non noisy functions

---
+++ name_utils.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Created on 26/07/2021 11:55
+# Author : matteo
+
+# ====================================================
+# imports
+
+
+# ====================================================
+# code
+class ShapeError(Exception):
+    """
+    Custom error for shape mismatches.
+    """
+    pass
