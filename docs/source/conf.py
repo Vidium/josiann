e74e8110fd8f42104196cae23a538a33f5462220
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2023-03-13 09:33:33 +0100

    requirements for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ extensions = [
     "sphinx.ext.napoleon",  # for parsing Numpy and Google style docstrings
     "sphinx_autodoc_typehints",  # for adding typehint parsing
     "nbsphinx",  # for integrating notebooks
+    "IPython.sphinxext.ipython_console_highlighting",  # for syntax highlight in notebooks
 ]
 
 nitpicky = True
@@ -51,24 +52,15 @@ nitpick_ignore = [
 
 
 # sphinx.ext.autodoc -- config ----------------------------
-
-# autoclass_content = 'class'
-
 autodoc_member_order = "bysource"
 autodoc_default_flags = ["members"]
 autoclass_content = "both"
 autodoc_typehints = "both"
 
-# autodoc_type_aliases = {
-#     'DType': 'josiann.typing.DType'
-# }
-
-# qualname_overrides = {
-#     "jot": "josiann.typing"
-# }
 
 # sphinx.ext.autosummary -- config ------------------------
 autosummary_generate = True
+
 
 # sphinx.ext.intersphinx -- config ------------------------
 intersphinx_mapping = dict(
@@ -76,12 +68,14 @@ intersphinx_mapping = dict(
     numpy=("https://numpy.org/doc/stable/", None),
 )
 
+
 # sphinx.ext.napoleon -- config ---------------------------
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 napoleon_include_init_with_doc = True
 napoleon_use_rtype = True  # having a separate entry generally helps readability
 napoleon_use_param = True
+
 
 # sphinx_autodoc_typehints -- config ----------------------
 typehints_defaults = "braces"
@@ -102,4 +96,4 @@ html_css_files = ["css/rtd_custom.css"]
 
 def setup(app: Sphinx):
     # Don’t allow broken links.
-    app.warningiserror = False
+    app.warningiserror = True
