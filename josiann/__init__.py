c715489327f053248b41c100f76d624ae7cca787
MatteoBouvierVidium <m.bouvier@vidium-solutions.com>
2023-04-17 10:15:38 +0200

    fixed import issues

--- josiann/__init__.py
+++ josiann/__init__.py
@@ -15,22 +15,13 @@ except ImportError:  # for Python<3.8
     import importlib_metadata as metadata  # type: ignore[import, no-redef]
 
 from josiann.algorithms.sequential.base.sa import sa
-from josiann.algorithms.sequential.vectorized.vsa import vsa
 from josiann.algorithms.sequential.multicore.mcsa import mcsa
-
+from josiann.algorithms.sequential.vectorized.vsa import vsa
+from josiann.moves.discrete import SetStep, SetStretch
+from josiann.moves.ensemble import Stretch, StretchAdaptive
+from josiann.moves.sequential import Metropolis, Metropolis1D, RandomStep
 from josiann.storage.result import Result
 from josiann.storage.trace import Trace
-
-from josiann.moves.sequential import RandomStep
-from josiann.moves.sequential import Metropolis
-from josiann.moves.sequential import Metropolis1D
-from josiann.moves.discrete import SetStep
-from josiann.moves.discrete import SetStretch
-from josiann.moves.ensemble import Stretch
-from josiann.moves.ensemble import StretchAdaptive
-
-import josiann.parallel
-
 
 # ====================================================
 # code
